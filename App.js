ea39bd6ca3818d07b875ac6ac61ce01bd3891eec
merv-e <34655454+merv-e@users.noreply.github.com>
2023-02-28 17:04:48 +0100

    project setup with some basic info and installation of packages

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Welcome from './src/screens/Welcome';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Welcome/>
     </View>
   );
 }
---
+++ src/screens/Welcome.js
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native-web"
+
+const Welcome = () => {
+  return (
+    <View>
+        <Text>Welcome to my app!</Text>
+    </View>
+  )
+}
+
+export default Welcome
