59e47ba09511e8de6bd88ae1e70077118890d643
merv-e <34655454+merv-e@users.noreply.github.com>
2023-03-06 17:46:44 +0100

    uncommented.

--- src/store/index.js
+++ src/store/index.js
@@ -1,18 +1,18 @@
 import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit'
 import reducer from "./reducers"
-// import rootSaga from "./sagas"
-// import createSagaMiddleware from '@redux-saga/core'
+import rootSaga from "./sagas"
+import createSagaMiddleware from '@redux-saga/core'
 
 const initializeStore = (preloadedState = {}) => {
-  // const sagaMiddleware = createSagaMiddleware();
+  const sagaMiddleware = createSagaMiddleware();
 
   const store = configureStore({
     reducer,
     preloadedState,
-  //   middleware : [...getDefaultMiddleware(), sagaMiddleware]
+    middleware : [...getDefaultMiddleware(), sagaMiddleware]
   })
 
-  // sagaMiddleware.run(rootSaga)
+  sagaMiddleware.run(rootSaga)
 
   return store
 }
