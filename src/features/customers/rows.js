8d9879aabcf213b10dba9063455c969609363218
merv-e <34655454+merv-e@users.noreply.github.com>
2023-03-15 20:53:41 +0100

    The problem with editing in the mobile as well as  seeing the app in the mobile environment is fixed.

--- src/features/customers/rows.js
+++ src/features/customers/rows.js
@@ -12,14 +12,12 @@ const Row = ({ item }) => {
 
     return (
         <TouchableOpacity onPress={() => navigate('Edit Customer', {customerID: item.id})}>
-            <View id={item.id}
-            style={{ borderWidth: 1, padding: 10, margin: 10 }}>
+           
                 <Text>ID: {item.id}</Text>
                 <Text>First Name: {item.firstName}</Text>
                 <Text>Last Name: {item.lastName}</Text>
                 <Text>User Activity: {item.isActive}</Text>
                 <Text>Region: {item.region}</Text>
-            </View>
         </TouchableOpacity>
     )
 }
--- src/features/notifications/index.js
+++ src/features/notifications/index.js
@@ -1,54 +1,137 @@
-import * as Notifications from "expo-notifications";
-import React, { useEffect } from "react";
-import { Button, Keyboard, Text, View } from "react-native";
+import *as Notifications from "expo-notifications";
+import React, { useEffect, useState, useRef } from "react"; 
+import { Button, Keyboard, Text, View, Platform } from "react-native";
 import * as Device from "expo-device";
 
-const onSubmit = (seconds) => {
-Keyboard.dismiss() 
+// const onSubmit = (seconds) => { 
+// Keyboard.dismiss() 
 
-Notifications.scheduleNotificationsAsync({
-   content: {
-      title: `Do not forget to contact ${customerName[Math.floor(Math.random()*customerName.length)]}`,
-      body: "Open the app to find out the customer you'll contact today.",
-      sound: true,
-      priority: Notifications.AndroidNotificationPriority.HIGH,
-      color: "black",
-  },
-  trigger: {
-      seconds: seconds,
-  }
-})
-};
-
-const handleNotification = () => {
-  console.warn('Your notification ran, but won`t show up in the app!')
-}
+//    content: {
+  //       title: `Do not forget to contact ${customerName[Math.floor(Math.random()*customerName.length)]}`,
+  //       body: "Open the app to find out the customer you'll contact today.",
+  //       sound: true,
+  //       priority: Notifications.AndroidNotificationPriority.HIGH,
+  //       color: "black",
+  //   },
+  //   trigger: {
+    //       seconds: seconds,
+    //   }
+    // })
 
 
-const askNotification = async () => {
-  const {status} = await Notifications.requestPermissionsAsync()
-      if(Device.isDevice && status === "granted")
-      console.log("Notification permissions granted.");
-};
+    // const handleNotification = () => {
+    //   console.warn('Your notification ran, but won`t show up in the app!')
+    // }
+    
+    
+    // const askNotification = async () => {
+    //   const {status} = await Notifications.requestPermissionsAsync()
+    //       if(Device.isDevice && status === "granted")
+    //       console.log("Notification permissions granted.");
+    // };
+    // };
+  
+    Notifications.setNotificationHandler({
+      handleNotification: async () => ({
+      shouldShowAlert: true,
+      shouldPlaySound: true,
+      shouldSetBadge: false,
+    }),
+  });
+
 
 const CustomerNotifications = () => {
-  useEffect(()=> {
-      askNotification();
+  
+  const [expoPushToken, setExpoPushToken] = useState('');
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
 
-      const listener = Notifications.addNotificationReceivedListener(handleNotification)
-      return () => listener.remove() 
+  useEffect(() => {
+    registerForPushNotificationsAsync()
+     .then(token =>
+       setExpoPushToken(token)
+      );
+
+    notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+      setNotification(notification);
+    });
+
+    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log(response);
+    });
+
+    return () => {
+      Notifications.removeNotificationSubscription(notificationListener.current);
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
   }, []);
 
-  // notification to be sent five sec after the app is closed.
   return (
-    <View>
-        <Text>Press the button to .... </Text>
-        <Button 
-            onPress={() => onSubmit(10)} 
-            title="Whatever"
-        />
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'space-around',
+      }}>
+      <Text>Your expo push token: {expoPushToken}</Text>
+      <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Title: {notification && notification.request.content.title} </Text>
+        <Text>Body: {notification && notification.request.content.body}</Text>
+        <Text>Data: {notification && JSON.stringify(notification.request.content.data)}</Text>
+      </View>
+      <Button
+        title="Press to schedule a notification"
+        onPress={async () => {
+          await schedulePushNotification();
+        }}
+      />
     </View>
-  )
+  );
+};
+
+
+async function schedulePushNotification() {
+  await Notifications.scheduleNotificationAsync({
+    content: {
+      body: 'You need to have a call with ${customer} ',
+      title: `Schedule a call`,
+      data: { data: 'goes here' },
+    },
+    trigger: { seconds: 5 },
+  });
+};
+
+async function registerForPushNotificationsAsync() {
+  let token;
+
+  if (Platform.OS === 'android') { 
+    await Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+    });
+  }
+
+  if (Device.isDevice) {
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+  //   if (finalStatus !== 'granted') {
+  //     alert('Failed to get push token for push notification!');
+  //     return;
+  //   }
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+    console.log(token);
+  // } else {
+  //   alert('Must use physical device for Push Notifications');
+  }
+
+  return token;
 };
 
 export default CustomerNotifications
